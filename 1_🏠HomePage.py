6d136db1c2ff41afe53963fababcec633d0ff707
Ashwin Patil <aspatil@microsoft.com>
2024-02-15 14:14:23 -0800

    adding notebook and changes

--- 1_🏠HomePage.py
+++ 1_🏠HomePage.py
@@ -8,7 +8,13 @@ def main() -> None:
     st.title(":mag_right: Interactive Anomaly detection using Isolation Forest")
 
     with st.expander("Expand to Read more about the Anomaly detection"):
-        st.write(Path("README.md").read_text())
+        # Load and display the README.md file
+        try:
+            with open('README.md', 'r', encoding='utf-8') as file:
+                readme_contents = file.read()
+                st.markdown(readme_contents, unsafe_allow_html=True)
+        except FileNotFoundError:
+            st.error('README.md file not found.')
 
     st.success(":point_left: Select a page on left side bar to naviagate pages")
 
